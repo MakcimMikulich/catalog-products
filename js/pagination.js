3d22139c6e3f4467b135acf4fc1aba2ed8ed9e8d
Makcim <adoberoflit@gmail.com>
2024-06-24 13:19:48 +0300

    add arrow pagination

--- js/pagination.js
+++ js/pagination.js
@@ -14,9 +14,11 @@ export function createPagination(totalPage) {
 	}
 
 	if (currentPage > 1) {
-		paginationHTML += `<div class="pagination__button" data-page="${
+		paginationHTML += `<div class="pagination__button with-arrow left" data-page="${
 			currentPage - 1
-		}">Prev</div>`;
+		}"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M8.78089 8.00047L5.48108 4.70062L6.42389 3.75781L10.6666 8.00047L6.42389 12.2431L5.48108 11.3003L8.78089 8.00047Z" fill="currentColor"/>
+			</svg></div>`;
 	}
 
 	let startPage = Math.max(currentPage - Math.floor(maxVisiblePages / 2), 1);
@@ -33,9 +35,11 @@ export function createPagination(totalPage) {
 	}
 
 	if (currentPage < totalPage) {
-		paginationHTML += `<div class="pagination__button" data-page="${
+		paginationHTML += `<div class="pagination__button with-arrow" data-page="${
 			currentPage + 1
-		}">Next</div>`;
+		}"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M8.78089 8.00047L5.48108 4.70062L6.42389 3.75781L10.6666 8.00047L6.42389 12.2431L5.48108 11.3003L8.78089 8.00047Z" fill="currentColor"/>
+			</svg></div>`;
 	}
 
 	pagination.innerHTML = paginationHTML;
